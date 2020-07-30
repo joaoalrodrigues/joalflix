14ef7fdf42ff972c87c6551dc8856101577bd3d0
joaoalrodrigues <joaoalrodrigues@users.noreply.github.com>
2020-07-30 09:24:51 +0200

    initial commit

---
+++ src/components/Carousel/components/Slider/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import SlickSlider from 'react-slick';
+import styled from 'styled-components';
+
+function SampleArrow(props) {
+    const { className, style, onClick, categoryColor } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: categoryColor }}
+            onClick={onClick}
+        />
+    );
+}
+
+const Container = styled.ul`
+padding: 0;
+margin: 0;
+.slick-prev,
+.slick-next {
+    z-index: 50;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    width: 30px;
+    height: 30px;
+    transform: initial;
+    padding-top:2px;
+
+    &:before {
+        font-size: 30px;
+    }
+}
+
+    .slick-prev{
+        left:0;
+    }
+
+    .slick-next{
+        right:16px;
+    }
+
+    .slick-list{
+        overflow:inherit;
+    }
+
+`;
+
+export const SliderItem = styled.li`
+margin-right: 16px;
+img {
+    margin: 16px;
+    width:298px;
+    height:197px;
+    object-fit: cover;
+}
+`;
+
+const Slider = ({ categoryColor, children }) => (
+    <Container>
+        <SlickSlider {...{
+            dots: false,
+            infinite: true,
+            speed: 300,
+            variableWidth: true,
+            adaptiveHeight: true,
+            centerMode: true,
+            nextArrow: <SampleArrow categoryColor={categoryColor} />,
+            prevArrow: <SampleArrow categoryColor={categoryColor} />
+        }}
+        >
+            {children}
+        </SlickSlider>
+
+    </Container>
+)
+
+export default Slider;
---
+++ src/components/Carousel/components/VideoCard/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const VideoCardContainer = styled.a`
+  border: 2px solid;
+  border-radius: 4px;
+  text-decoration: none;
+  cursor: pointer;
+  color: white;
+  flex: 0 0 298px;
+  width: 298px;
+  min-height: 197px;
+  background-image: ${({ url }) => `url(${url})`};
+  background-size: cover;
+  background-position: center;
+  border-radius: 10px;
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  padding: 16px;
+  opacity: .5;
+
+  transition: opacity .3s;
+  &:hover,
+  &:focus {
+    opacity:.8;
+    width: 350px;
+    height: 250px;
+  }
+  
+  &:not(:first-child) {
+    margin-left: 20px;
+  }
+`;
