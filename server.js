53f3e889a091269303e2b53eb1879fefecc6dd08
joaoalrodrigues <joaoalrodrigues@users.noreply.github.com>
2020-08-15 00:01:42 +0200

    added form to register new channels

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const channelRouter = require('./router/channelRouter');
 dotenv.config({ path: './config.env' });
 
 server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
 
